cdbc28f5058b52dfb5109e2faaaee084a3f8b80d
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-04 22:40:20 +0530

    [Prasath] added loop to run continuously

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
-coinflip=$(( RANDOM%2 ))
-if [ $coinflip -eq 0 ]
-then
-        echo "Heads"
-else
-        echo "Tails"
-fi
+counter=0
+count=0
+count1=0
+while [ $counter -lt 10 ]
+do
+	coinflip=$(( RANDOM%2 ))
+	if [ $coinflip -eq 0 ]
+	then
+        	echo "Heads"
+	else
+        	echo "Tails"
+	fi
+counter=$(( $counter+1 ))
+done
+echo "number of heads="$count
+echo "number of tails="$count1
+
